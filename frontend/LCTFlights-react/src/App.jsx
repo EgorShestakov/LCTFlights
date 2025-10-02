a9c0068e1249771db1f24b8498e58450531ee3cc
DimaP0 <dima.podaryashchiy@gmail.com>
2025-10-02 15:41:14 +0400

    Fixed API routes tested by deploying to the server

---
+++ frontend/LCTFlights-react/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import "./App.css";
+
+
+import Redirect from "./Shared/Redirect.jsx";
+import MapPage from "./Pages/MapPage/MapPage.jsx";
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/map" element={<MapPage/>}/>
+                    <Route path="/:region/satat" element={<About/>}/>
+                    <Route path="/reports" element={<Contact/>}/>
+                    <Route path="/*" element={<Redirect to={'/map'}/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+const Home = () => <h2>Home Page</h2>;
+const About = () => <h2>About Page</h2>;
+const Contact = () => <h2>Contact Page</h2>;
+
+export default App;
